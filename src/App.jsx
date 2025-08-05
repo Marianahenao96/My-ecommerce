e27e437e2c25bda570108099f4d633b1692c5c2f
Marianahenao96 <mari6599@hotmail.com>
2025-08-05 13:12:14 -0500

    agrega cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './components/NavBar/navbar.css';  
+import './components/NavBar/NavBar.css';  
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
